789c2a559fb3371fe64f80cac346d13cd4d2c73a
Antoine Allard <admin@ycraft.fr>
2018-01-20 16:16:03 +0100

    Adding messages in console when enabling or desabling

--- src/fr/fuizziy/reputation/Reputation.java
+++ src/fr/fuizziy/reputation/Reputation.java
@@ -1,5 +1,19 @@
 package fr.fuizziy.reputation;
 
-public class Reputation {
+import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
+public class Reputation extends JavaPlugin {
+
+	public void onEnable() {
+		getServer().getConsoleSender().sendMessage(String.format(ChatColor.GREEN + "[FY] [%s v.%s] " + 
+	ChatColor.BLUE + " activé", getDescription().getName(), getDescription().getVersion()));
+	}
+	
+	public void onDisable() {
+		getServer().getConsoleSender().sendMessage(String.format(ChatColor.GREEN + "[FY] [%s v.%s] " + 
+	ChatColor.RED + " désactivé", getDescription().getName(), getDescription().getVersion()));
+	}
+	
 }
